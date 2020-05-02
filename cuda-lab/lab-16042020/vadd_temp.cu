01aa3563489a3932889385adb1e2349f4987d17d
cuda lab07 <cuda-lab07@nat110-fix-221.fis.agh.edu.pl>
2020-05-02 22:00:47 +0200

    Result

--- cuda-lab/lab-16042020/vadd_temp.cu
+++ cuda-lab/lab-16042020/vadd_temp.cu
@@ -54,14 +54,21 @@ int main()
   float *a;
   float *b;
   float *c;
+	float* d;
+	float* e;
 
   cudaMallocManaged(&a, size);
   cudaMallocManaged(&b, size);
   cudaMallocManaged(&c, size);
+	cudaMallocManaged(&d, size);
+	cudaMallocManaged(&e, size);
+
 
   cudaMemPrefetchAsync(a, size, deviceId);
   cudaMemPrefetchAsync(b, size, deviceId);
   cudaMemPrefetchAsync(c, size, deviceId);
+	cudaMemPrefetchAsync(d, size, deviceId);
+	cudaMemPrefetchAsync(e, size, deviceId);
 
   size_t threadsPerBlock;
   size_t numberOfBlocks;
@@ -93,9 +100,12 @@ int main()
   //initWith<<<numberOfBlocks, threadsPerBlock>>>(0, c, N);
   
 	
-  addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream1>>>(c, a, b, N);
-  //addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream2>>>(c+N/3,a+N/3, b+N/3, N/3);
-  //addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream3>>>(c+2*N/3, a+2*N/3, b+2*N/3, N/3); 
+  //addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream1>>>(c, a, b, N/3);
+  //addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream2>>>(c+N/3,a+N/3, b+N/3, N/3 + 1);
+  //addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream3>>>(c+2*N/3, a+2*N/3, b+2*N/3, N/3 + 1); 
+	addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream1>>>(c, a, b, N);
+	addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream2>>>(d, a, b, N);
+	addVectorsInto<<<numberOfBlocks, threadsPerBlock, 0, stream3>>>(e, a, b, N);
 
   addVectorsErr = cudaGetLastError();
   if(addVectorsErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(addVectorsErr));
